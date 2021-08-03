46b4f7761972ad7ecf4fd50004ea51cf46244b69
JMcB <joel.mcbride1@live.com>
2021-08-03 13:37:44 +0100

    Write some functions sure

--- theme_converter/__init__.py
+++ theme_converter/__init__.py
@@ -1,5 +1,30 @@
 import configparser
+from pathlib import Path
+
+import requests
 from bs4 import BeautifulSoup
+
+
+DEFAULTCOLORSCHEMESMANAGER_XML_URL = 'https://raw.githubusercontent.com/JetBrains/intellij-community/master' \
+                                     '/platform/platform-resources/src/DefaultColorSchemesManager.xml'
+
+
+def download_defaults(defaults_url: str = DEFAULTCOLORSCHEMESMANAGER_XML_URL) -> BeautifulSoup:
+    """Get the default colour schemes xml from the IntelliJ Community GitHub repo and put it in a soup."""
+    response = requests.get(defaults_url)
+    soup = BeautifulSoup(response.raw)
+    return soup
+
+
+def save_idle_theme(theme_name: str, theme: dict, config_highlight_path: Path):
+    """Save the IDLE theme to the config-highlight.cfg file with name theme_name."""
+    config_highlight = configparser.ConfigParser()
+    config_highlight.read(config_highlight_path, encoding='utf-8')
+
+    # todo: pay attention to overwrites
+    config_highlight[theme_name] = theme
+    with open(config_highlight_path, 'w', encoding='utf-8') as config_highlight_file:
+        config_highlight.write(config_highlight_file, space_around_delimiters=False)
 
 
 def main():
