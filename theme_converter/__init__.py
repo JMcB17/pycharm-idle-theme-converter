73a0491565e3da8ac1526730e6c8fcc0c9dc25b1
JMcB <joel.mcbride1@live.com>
2021-08-05 16:17:30 +0100

    Implement pycharm theme conversion, and testing

--- theme_converter/__init__.py
+++ theme_converter/__init__.py
@@ -1,12 +1,28 @@
+import argparse
 import configparser
+import json
 from pathlib import Path
 
 import requests
 from bs4 import BeautifulSoup
 
+import theme_converter.converters.pycharm
+
 
 DEFAULTCOLORSCHEMESMANAGER_XML_URL = 'https://raw.githubusercontent.com/JetBrains/intellij-community/master' \
                                      '/platform/platform-resources/src/DefaultColorSchemesManager.xml'
+MAPPINGS_DIRECTORY = Path('mappings/')
+IDLERC_DIRECTORY = Path.home() / '.idlerc'
+CONFIG_MAIN_CFG = IDLERC_DIRECTORY / 'config-highlight.cfg'
+
+
+def get_parser() -> argparse.ArgumentParser:
+    """Get the parser for the main function."""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('icls_file', type=Path)
+
+    return parser
 
 
 def download_defaults(defaults_url: str = DEFAULTCOLORSCHEMESMANAGER_XML_URL) -> BeautifulSoup:
@@ -24,8 +40,20 @@ def save_idle_theme(theme_name: str, theme: dict, config_highlight_path: Path):
     # todo: pay attention to overwrites
     config_highlight[theme_name] = theme
     with open(config_highlight_path, 'w', encoding='utf-8') as config_highlight_file:
-        config_highlight.write(config_highlight_file, space_around_delimiters=False)
+        config_highlight.write(config_highlight_file)
 
 
 def main():
-    pass
+    parser = get_parser()
+    args = parser.parse_args()
+
+    icls_file = args.icls_file
+    icls_contents = icls_file.read_text()
+    icls_soup = BeautifulSoup(icls_contents)
+    mapping_path = MAPPINGS_DIRECTORY / 'pycharm.json'
+    with open(mapping_path, encoding='utf-8') as mapping_file:
+        pycharm_mapping = json.load(mapping_file)
+    pycharm_default_themes = download_defaults()
+
+    new_theme = theme_converter.converters.pycharm.convert(icls_soup, pycharm_default_themes, pycharm_mapping)
+    save_idle_theme('test', new_theme, CONFIG_MAIN_CFG)
--- theme_converter/converters/pycharm.py
+++ theme_converter/converters/pycharm.py
@@ -1,26 +1,37 @@
-from bs4 import BeautifulSoup
+from typing import Union, List
+from bs4 import BeautifulSoup, Tag
 
 
-def add_idle_key_attribute(idle_key: str, pycharm_option: BeautifulSoup, idle_theme: dict):
-    idle_theme[f'{idle_key}-foreground'] = pycharm_option.find(name='FOREGROUND')
-    idle_theme[f'{idle_key}-background'] = pycharm_option.find(name='BACKGROUND')
+def _find_key_with_fallbacks(themes: Union[Tag, List[Tag]], keys: Union[str, List[str]]) -> Tag:
+    """Find the first result from a number of themes and a number of keys, searching keys first then themes."""
+    if not isinstance(themes, list):
+        themes = [themes]
+    if not isinstance(keys, list):
+        keys = [keys]
+
+    # todo: handle failure here
+    for theme in themes:
+        for key in keys:
+            tag_search = theme.find(key)
+            if tag_search:
+                return tag_search
 
 
 def convert(theme: BeautifulSoup, defaults: BeautifulSoup, mapping: dict) -> dict:
     """Convert an IntelliJ IDEA .icls theme to a dict for configparser."""
     idle_theme = {}
 
+    # todo: handle failure here
     parent_theme_name = theme.scheme.parent_scheme
     parent_theme = defaults.find(parent_theme_name)
 
-    for idle_key, pycharm_key in mapping['attributes'].values():
-        if isinstance(pycharm_key, list):
-            pass
-        else:
-            theme_get = theme.find(pycharm_key)
-            if theme_get:
-                add_idle_key_attribute(idle_key, theme_get, idle_theme)
-            else:
-                add_idle_key_attribute(idle_key, parent_theme.find(pycharm_key), idle_theme)
+    for idle_key, pycharm_key in mapping['colors'].values():
+        pycharm_color_tag = _find_key_with_fallbacks([theme, parent_theme], pycharm_key)
+        idle_theme[idle_key] = pycharm_color_tag.value
+
+    for idle_key_partial, pycharm_key in mapping['attributes'].values():
+        pycharm_attribute_tag = _find_key_with_fallbacks([theme, parent_theme], pycharm_key)
+        idle_theme[f'{idle_key_partial}-foreground'] = pycharm_attribute_tag.find(name='FOREGROUND')
+        idle_theme[f'{idle_key_partial}-background'] = pycharm_attribute_tag.find(name='BACKGROUND')
 
     return idle_theme
