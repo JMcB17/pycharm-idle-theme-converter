6c3ad03e9321b829fa8ca3b2cc6af6466c4491a8
JMcB <joel.mcbride1@live.com>
2021-08-06 16:57:34 +0100

    Finish pycharm converter implementation

--- theme_converter/converters/pycharm.py
+++ theme_converter/converters/pycharm.py
@@ -1,20 +1,31 @@
 from typing import Union, List
-from bs4 import BeautifulSoup, Tag
+from bs4 import BeautifulSoup
+from bs4.element import Tag  # type hints
 
 
-def _find_key_with_fallbacks(themes: Union[Tag, List[Tag]], keys: Union[str, List[str]]) -> Tag:
+# todo: fix mapping for error, the pycharm key it's mapped to doesn't have fg or bg but does have effect
+
+
+ATTR_MAPPING = {'-foreground': 'FOREGROUND', '-background': 'BACKGROUND'}
+
+
+def _find_key_with_fallbacks(themes: Union[Tag, List[Tag]], keys: Union[str, List[str]]) -> List[Tag]:
     """Find the first result from a number of themes and a number of keys, searching keys first then themes."""
     if not isinstance(themes, list):
         themes = [themes]
     if not isinstance(keys, list):
         keys = [keys]
+    # always fall back to Default Text
+    keys.append('TEXT')
 
-    # todo: handle failure here
+    hits = []
     for theme in themes:
         for key in keys:
             tag_search = theme.find('option', attrs={'name': key})
-            if tag_search:
-                return tag_search
+            if tag_search is not None:
+                hits.append(tag_search)
+
+    return hits
 
 
 def convert(theme: BeautifulSoup, defaults: BeautifulSoup, mapping: dict) -> dict:
@@ -26,22 +37,31 @@ def convert(theme: BeautifulSoup, defaults: BeautifulSoup, mapping: dict) -> dic
     parent_theme = defaults.find('scheme', attrs={'name': parent_theme_name})
 
     for idle_key, pycharm_key in mapping['colors'].items():
-        pycharm_color_tag = _find_key_with_fallbacks([theme, parent_theme], pycharm_key)
-        idle_theme[idle_key] = pycharm_color_tag['value']
+        pycharm_color_tags = _find_key_with_fallbacks([theme, parent_theme], pycharm_key)
+        # this is needed exclusively because SELECTION_FOREGROUND has a value field but it's empty
+        # pycharm's theme system is not very cool
+        for color_tag in pycharm_color_tags:
+            try:
+                if color_tag['value']:
+                    idle_theme[idle_key] = color_tag['value']
+                    break
+            except KeyError:
+                for idle_suffix, pycharm_attr in ATTR_MAPPING.items():
+                    if idle_key.endswith(idle_suffix):
+                        idle_theme[idle_key] = color_tag.find('option', attrs={'name': pycharm_attr})['value']
 
     for idle_key_partial, pycharm_key in mapping['attributes'].items():
-        pycharm_attribute_tag = _find_key_with_fallbacks([theme, parent_theme], pycharm_key)
+        pycharm_attribute_tags = _find_key_with_fallbacks([theme, parent_theme], pycharm_key)
 
-        # todo: need fallback on these too
-        # todo: remove debug fallback
-        foreground_option = pycharm_attribute_tag.find('option', attrs={'name': 'FOREGROUND'}) or {'value': ''}
-        foreground_color = foreground_option['value']
-        idle_theme[f'{idle_key_partial}-foreground'] = foreground_color
+        for idle_suffix, pycharm_attr in ATTR_MAPPING.items():
+            for attribute_tag in pycharm_attribute_tags:
+                tag_search = attribute_tag.find('option', attrs={'name': pycharm_attr})
+                if tag_search is not None:
+                    idle_theme[idle_key_partial + idle_suffix] = tag_search['value']
+                    break
 
-        background_option = pycharm_attribute_tag.find('option', attrs={'name': 'BACKGROUND'}) or {'value': ''}
-        background_color = background_option['value']
-        idle_theme[f'{idle_key_partial}-background'] = background_color
-
-    # todo: add hashes to color codes, idle themes have them but not pycharm themes
+    # convert colour codes from pycharm format to idle format
+    for key, value in idle_theme.items():
+        idle_theme[key] = '#' + value.upper()
 
     return idle_theme
